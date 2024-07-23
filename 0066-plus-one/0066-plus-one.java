f9b96a9a38a339d01458acd4608501a28ab49972
Mark Popov <148067937+Markelloww@users.noreply.github.com>
2024-07-23 18:06:22 +0300

    Time: 0 ms (100%), Space: 41.8 MB (23.38%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Author: Markelloww
  * Date: 23.07.2024
@@ -8,29 +5,16 @@ import java.util.List;
 
 public class Solution {
     public int[] plusOne(int[] digits) {
-
-        List<Integer> arrayList = new ArrayList<>();
-        for (int digit : digits)
-            arrayList.add(digit);
-
-        int index = arrayList.size() - 1;
-        while (index >= 0) {
-            if (arrayList.get(index) == 9) {
-                arrayList.set(index, 0);
-            } else {
-                arrayList.set(index, arrayList.get(index) + 1);
-                break;
+        int size = digits.length;
+        for (int i = size - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
             }
-            index--;
+            digits[i] = 0;
         }
-        if (arrayList.get(0) == 0) {
-            arrayList.add(0, 1);
-        }
-
-        int[] newArr = new int[arrayList.size()];
-        for (int i = 0; i < arrayList.size(); i++)
-            newArr[i] = arrayList.get(i);
-        return newArr;
-
+        int[] newDigits = new int[size + 1];
+        newDigits[0] = 1;
+        return newDigits;
     }
 }
