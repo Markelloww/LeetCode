645cfe5f7fd5e0af54c2bc64dde240d07da97236
Mark Popov <148067937+Markelloww@users.noreply.github.com>
2024-07-23 18:59:57 +0300

    Time: 1 ms (98.92%), Space: 41.6 MB (20.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+/**
+ * Author: Markelloww
+ * Date: 23.07.2024
+ */
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '(', '{', '[':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
