3cea43d9c78876ecaf7f833923b4b857187ced4e
Mark Popov <148067937+Markelloww@users.noreply.github.com>
2024-07-23 16:24:52 +0300

    Time: 0 ms (100%), Space: 41.6 MB (25.41%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+/**
+ * Author: Markelloww
+ * Date: 23.07.2024
+ */
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
